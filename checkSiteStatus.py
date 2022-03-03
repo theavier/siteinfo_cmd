b0d649f8c3bf5e1853e681708cbace1bd011effa
theavier <theavier@gmail.com>
2022-03-03 11:27:48 +0100

    updated error handling

--- checkSiteStatus.py
+++ checkSiteStatus.py
@@ -17,14 +17,24 @@ def write_csv(_items: list, output: str) -> None:
         writer.writeheader()
         writer.writerows(_items)
 
+def validate_http(url):
+    if url.startswith("https://") or url.startswith("http://"):
+        return url
+    else:
+        return f'https://{url}'
 
 def add_statuscode(_items: list) -> list:
     results = list()
     for item in _items:
-        r = requests.get(item['url'], stream=True)
-        ip = r.raw._connection.sock.getpeername()[0]
-        item['statuscode'] = r.status_code
-        item['ip'] = ip
+        try:
+            r = requests.get(validate_http(item['url']), stream=True)
+            ip = r.raw._connection.sock.getpeername()[0]
+            item['statuscode'] = r.status_code
+            item['ip'] = ip
+        except:
+            print(f'{item["url"]}: something went wrong')
+            item['statuscode'] = "N/A"
+            item['ip'] = "N/A"
         results.append(item)
     return results
 

3bad9df325d326f4be0f95363253a12fa0d1b6b1
theavier <theavier@gmail.com>
2023-01-09 09:34:53 +0100

    Optimized code
    updated all with functools partial solution

--- README.md
+++ README.md
@@ -1,26 +1,26 @@
 Python check for status of urls. Returns ip, statuscode
 
 Usage:
-/> python siteinfo.py status --csv input_csv --output results.json
+/> python siteinfo.py --csv input_csv --output results.json status 
 
 Outputs to --output filename. If no value given, prints results
 
 Python check for whois attributes. Returns ['registrant_name', 'expiration_date', 'registrar', 'name_servers'] if available.
 Usage:
-/> python siteinfo.py whois --csv input_csv --output results.json
+/> python siteinfo.py --csv input_csv --output results.json whois 
 
 Check only one site.
 /> python siteinfo.py whois mydomain.com 
 
 Check for what is running. Returns json 
 Usage:
-/> python siteinfo.py whatis --csv input.csv --output results.json
+/> python siteinfo.py --csv input.csv --output results.json whatis 
 
 Check ip address resolves to
-/> python siteinfo.py status --csv input.csv --output results.json
+/> python siteinfo.py --csv input.csv --output results.json status 
 
 Check it all
-/> python siteinfo.py all --csv input.csv --output results.json 
+/> python siteinfo.py --csv input.csv --output results.json all  
 
 Check params
 /> python siteinfo.py --help
--- siteinfo.py
+++ siteinfo.py
@@ -5,84 +5,76 @@ from checkSiteStatus import add_statuscode
 from whereis import whereis_items
 from loguru import logger
 from tools import merge_list_dicts, log_init, list_or_item, save_or_print
+from functools import partial
 
 app = typer.Typer()
-
+state = {"verbose": False, "csv": None, "output": None}
 
 @app.command('all')
-def run_main(url: str = typer.Argument(None, help='url to scan'),
-            csv: str = typer.Option(None, help='csv with urls to scan'),
-            output: str = typer.Option(None, help='output filename'),
-            verbose: bool = typer.Option(True)) -> None:
+def run_main(url: str = typer.Argument(None, help='url to scan')) -> None:
     """ Runs whois, ip lookup and whatcms on url(s) """
-    log_init(verbose)
-    items = list_or_item(url, csv)
+    log_init(state['verbose'])
+    items = list_or_item(url, state['csv'])
+
+    all_functions = [partial(whois_domain_items), partial(add_statuscode), partial(whatis_items),
+                     partial(whereis_items)]
     end_results = list()
+    for all_function in all_functions:
+        results_func = all_function(items)
+        end_results = merge_list_dicts(end_results, results_func, "url")
 
-    # add whois results
-    results_whois = whois_domain_items(items)
-    end_results = merge_list_dicts(end_results, results_whois, "url")
-
-    # add status results
-    results_status = add_statuscode(items)
-    end_results = merge_list_dicts(end_results, results_status, "url")
-
-    # add whatis results
-    results_whatis = whatis_items(items)
-    end_results = merge_list_dicts(end_results, results_whatis, "url")
-
-    # add whereis results
-    results_whereis = whereis_items(items)
-    end_results = merge_list_dicts(end_results, results_whereis, "url")
-
-    save_or_print(end_results, output)
+    save_or_print(end_results, state['output'])
 
 
 @app.command('whereis')
-def whereis_main(url: str = typer.Argument(None, help='url to scan'),
-    csv: str = typer.Option(None, help='csv with urls to scan'),
-    output: str = typer.Option(None, help='output filename'),
-    verbose: bool = typer.Option(False)) -> None:
+def whereis_main(url: str = typer.Argument(None, help='url to scan')) -> None:
     """ Runs ip provider lookup on url """
-    log_init(verbose)
-    items = list_or_item(url, csv)
+    log_init(state['verbose'])
+    items = list_or_item(url, state['csv'])
     end_results = whereis_items(items)
-    save_or_print(end_results, output)
+    save_or_print(end_results, state['output'])
+
 
 @app.command('whatis')
-def whatis_main(url: str = typer.Argument(None, help='url to scan'),
-    csv: str = typer.Option(None, help='csv with urls to scan'),
-    output: str = typer.Option(None, help='output filename'),
-    verbose: bool = typer.Option(False)) -> None:
+def whatis_main(url: str = typer.Argument(None, help='url to scan')) -> None:
     """ Runs whatcms on url """
-    log_init(verbose)
-    items = list_or_item(url, csv)
+    log_init(state['verbose'])
+    items = list_or_item(url, state['csv'])
     end_results = whatis_items(items)
-    save_or_print(end_results, output)
+    save_or_print(end_results, state['output'])
 
 
 @app.command('status')
-def status_main(url: str = typer.Argument(None, help='url to scan'),
-    csv: str = typer.Option(None, help='csv with urls to scan'),
-    output: str = typer.Option(None, help='output filename'),
-    verbose: bool = typer.Option(False)) -> None:
+def status_main(url: str = typer.Argument(None, help='url to scan')) -> None:
     """ Runs ip lookup on url """
-    log_init(verbose)
-    items = list_or_item(url, csv)
+    log_init(state['verbose'])
+    items = list_or_item(url, state['csv'])
     end_results = add_statuscode(items)
-    save_or_print(end_results, output)
+    save_or_print(end_results, state['output'])
 
 
 @app.command('whois')
-def whois_main(url: str = typer.Argument(None, help='url to scan'),
-             csv: str = typer.Option(None, help='csv with urls to scan'),
-             output: str = typer.Option(None, help='output filename'),
-             verbose: bool = typer.Option(False)) -> None:
+def whois_main(url: str = typer.Argument(None, help='url to scan')) -> None:
     """ Runs whois on url """
-    log_init(verbose)
-    items = list_or_item(url, csv)
+    log_init(state['verbose'])
+    items = list_or_item(url, state['csv'])
     results = whois_domain_items(items)
-    save_or_print(results, output)
+    save_or_print(results, state['output'])
+
+
+@app.callback()
+def main(csv: str = typer.Option(None, help='csv with urls to scan'),
+         output: str = typer.Option(None, help='output filename'),
+         verbose: bool = typer.Option(False, help='show verbose')) -> None:
+    """ General site information tools """
+    if verbose:
+        state['verbose'] = True
+    if csv:
+        state['csv'] = csv
+    if output:
+        state['output'] = output
+
+
 
 
 if __name__ == '__main__':
